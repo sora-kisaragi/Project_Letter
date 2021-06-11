ae7ccce3f6353d4386c7ada1379b77f7b3deb484
Sora_Kisaragi <soramunehiro.game@gmail.com>
2021-06-11 22:51:32 +0900

    Speach to Event ver-win

--- Assets/Scripts/Other/Title.cs
+++ Assets/Scripts/Other/Title.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
 
 public class Title : MonoBehaviour
 {
     public Text test;
+    private KeywordController keyCon;
+    private string[][] keywords;
+
+    private bool isStartFlag = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        keywords = new string[2][];
+        keywords[0] = new string[] { "シンシアリ", "sincerely" };//ひらがなでもカタカナでもいい
+        keywords[1] = new string[] { "スタート", "Start", "起動", "きどう" };
+
+        keyCon = new KeywordController(keywords, true);//keywordControllerのインスタンスを作成
+        keyCon.SetKeywords();//KeywordRecognizerにkeywordsを設定する
+        keyCon.StartRecognizing(0);//シーン中で音声認識を始めたいときに呼び出す
+        keyCon.StartRecognizing(1);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        CheckVoice();
+
         if (Input.GetMouseButton(0))
         {
             test.text = "Go to Next Scene";
@@ -37,6 +54,26 @@ public class Title : MonoBehaviour
             }
         }
 
+    }
+
+    void CheckVoice()
+    {
+        if (keyCon.hasRecognized[0])//設定したKeywords[0]の単語らが認識されたらtrueになる
+        {
+            Debug.Log("keyword[0] was recognized");
+            keyCon.hasRecognized[0] = false;
+            isStartFlag = true;
+        }
+        if (keyCon.hasRecognized[1])
+        {
+            Debug.Log("keyword[1] was recognized");
+            keyCon.hasRecognized[1] = false;
+            if (isStartFlag)
+            {
+                test.text = "Go to Next Scene";
+                SceneManager.LoadScene("Main_ios");
+            }
+        }
     }
 
 
--- Assets/Scripts/Recognize/KeywordTest.cs
+++ Assets/Scripts/Recognize/KeywordTest.cs
@@ -8,7 +8,7 @@ public class KeywordTest : MonoBehaviour {
 
     private KeywordController keyCon;
     private string[][] keywords;
-    private BoyomichanClient boyomi;
+
 
 
     // Use this for initialization
@@ -22,9 +22,6 @@ public class KeywordTest : MonoBehaviour {
         keyCon.StartRecognizing(0);//シーン中で音声認識を始めたいときに呼び出す
         keyCon.StartRecognizing(1);
 
-        boyomi = new BoyomichanClient("127.0.0.1", 50001);
-
-        
     }
 
     // Update is called once per frame
@@ -32,13 +29,15 @@ public class KeywordTest : MonoBehaviour {
         if (keyCon.hasRecognized[0])//設定したKeywords[0]の単語らが認識されたらtrueになる
         {
             Debug.Log("keyword[0] was recognized");
+
+            if (keyCon.hasRecognized[1])
+            {
+                Debug.Log("keyword[1] was recognized");
+                keyCon.hasRecognized[1] = false;
+            }
             keyCon.hasRecognized[0] = false;
         }
-        if (keyCon.hasRecognized[1])
-        {
-            Debug.Log("keyword[1] was recognized");
-            keyCon.hasRecognized[1] = false;
-        }
+
     }
 
 
